64336c53e5c111bef50a1a8e7955917c0fcf8128
Annenkov Aleksei <144391277+alekseiannenkov@users.noreply.github.com>
2025-01-19 21:27:00 +0300

    Add files via upload

---
+++ hw1/Program.cs
@@ -0,0 +1,36 @@
+﻿using PedalCarAccountingInformationSystem;
+using System.Xml.Linq;
+
+namespace PedalCarAccauntingInformationSystem;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var customers = new List<Customer>
+        {
+            new Customer("Alex"),
+            new Customer("Boris"),
+            new Customer("Dmitry"),
+            new Customer("Kirill"),
+            new Customer("Le Dat")
+        };
+
+        var factory = new FactoryAF(customers);
+
+        for (int i = 0; i < 5; i++)
+        {
+            factory.AddCar();
+        }
+
+        Console.WriteLine("Before");
+        Console.WriteLine(string.Join(Environment.NewLine, factory.Cars));
+        Console.WriteLine(string.Join(Environment.NewLine, factory.Customers));
+
+        factory.SaleCar();
+
+        Console.WriteLine("After");
+        Console.WriteLine(string.Join(Environment.NewLine, factory.Cars));
+        Console.WriteLine(string.Join(Environment.NewLine, factory.Customers));
+    }
+}
